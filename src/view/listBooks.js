8fa3e568f20133a5ea90de068cf9001e3864489a
Kristian Tagesen <kristiantagesen@MacBook-Pro.HYBEL>
2016-09-17 12:36:46 +0200

    Added more ctrl and corresponding html

--- src/view/listBooks.js
+++ src/view/listBooks.js
@@ -8,6 +8,10 @@ pl.view.listBooks = {
     //for each book, create table row with cell for each attribute
     for (i=0; i<keys.length;i++){
       key=keys[i];
+      row = tableBodyEl.insertRow();
+      row.insertCell(-1).textContent = Book.instances[key].isbn;
+      row.insertCell(-1).textContent = Book.instances[key].title;
+      row.insertCell(-1).textContent = Book.instances[key].year;
     }
   }
 };
