b84be913c9cbcb6ae8766ed81158618ec8479d03
Kristian Tagesen <kristiantagesen@MacBook-Pro.HYBEL>
2016-09-17 12:50:26 +0200

    Final

--- src/view/updateBook.js
+++ src/view/updateBook.js
@@ -10,6 +10,37 @@ pl.view.updateBook = {
       key=keys[i];
       book = Book.instances[key];
       optionEl = document.createElement("option");
+      optionEl.text = book.title;
+      optionEl.value = book.isbn;
+      selectBookEl.add( optionEl, null);
     }
-  };
+    //when book is selected, populate form with book data
+    selectBookEl.addEventListener("change", function() {
+      var book=null, key=selectBookEl.value;
+      if(key) {
+        book = Book.instances[key];
+        formEl.isbn.value = book.isbn;
+        formEl.title.value = book.title;
+        formEl.year.value = book.year;
+      } else {
+        formEl.isbn.value = "";
+        formEl.title.value="";
+        formEl.year.value="";
+      }
+    });
+    saveButton.addEventListener("click", pl.view.updateBook.handleUpdateButtonClickEvent);
+    window.addEventListener("beforeunload", function() {
+      Book.saveAll();
+    });
+  },
+  //save updated data
+  handleUpdateButtonClickEvent: function () }{
+    var formEl = document.forms['Book'];
+    var slots = {isbn: formEl.isbn.value,
+        title: formEl.title.value,
+        year: formEl.year.value
+      };
+    Book.update( slots);
+    formEl.reset();
+  }
 };
